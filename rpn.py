4fe6caafa76d3f38b03aaf17841ed32c3c3192c6
Nathan M. Podolsky <nathanmp@umich.edu>
2018-10-26 11:20:16 -0400

    Display calculator output.

--- rpn.py
+++ rpn.py
@@ -21,7 +21,8 @@ def calculate(arg):
 
 def main():
     while True:
-        calculate(input('rpn calc> '))
+        result = calculate(input('rpn calc> ')))
+        print(result)
 
 if __name__ == '__main__':
     main()
